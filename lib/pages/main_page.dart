ff874057d26104a204b5a9882c8858a444fb7639
Maksim Levchenko <valvekat@mail.ru>
2024-03-12 13:00:22 +0300

    Add settings loading logic to MainPage

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,12 +1,39 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:lightshot_parser_mobile/pages/settings_page.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
 class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+  MainPage({super.key});
+
+  late int numOfImages;
+  late bool newAddresses;
+  late String startingUrl;
 
   Widget _gallery() {
     return Placeholder();
+  }
+
+  Future<Directory> _appDocDir = getApplicationDocumentsDirectory();
+
+  void _loadSettings() {
+    _appDocDir.then((value) {
+      final File file = File('${value.path}/settings.json');
+      if (file.existsSync()) {
+        final String jsonString = file.readAsStringSync();
+        final Map<String, dynamic> settings = json.decode(jsonString);
+        //numOfImages = int.parse(settings[]);
+        //newAddresses = settings[bool.parse(settings['newAddresses'])];
+        //startingUrl = settings['startingUrl'];
+      } else {
+        numOfImages = 10;
+        newAddresses = false;
+        startingUrl = '';
+      }
+    });
   }
 
   @override
