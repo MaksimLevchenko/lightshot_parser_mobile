e4a5e42549736c18098a8f57cb50aebbc37775a3
Maksim Levchenko <valvekat@mail.ru>
2024-04-05 19:48:40 +0300

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:lightshot_parser_mobile/pages/main_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: const MainPage(),
+    home: MainPage(),
     theme: ThemeData.from(
         useMaterial3: false,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)),
