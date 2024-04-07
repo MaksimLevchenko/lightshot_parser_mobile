374381fd7cdc1d02ce0ee2ab5e100fbaab09c60e
EliteNoobest <Valvekat@mail.ru>
2024-04-07 05:03:29 +0300

    Refactor LightshotParser class and improve image downloading logic

--- lib/parser/parser.dart
+++ lib/parser/parser.dart
@@ -1,32 +1,42 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import "dart:core";
-import 'url_generator.dart' as gen;
 import 'parser_db.dart' as db;
 
+class CouldntConnectException implements Exception {
+  String cause;
+  CouldntConnectException(this.cause);
+}
+
+class NoPhotoException implements Exception {
+  String cause;
+  NoPhotoException(this.cause);
+}
+
 class LightshotParser {
-  bool downloading = true;
   bool alreadyExists = false;
   late final db.DataBase database;
   late final Directory photosDirectory;
   late final Directory databaseDirectory;
   static final LightshotParser _instance = LightshotParser._();
+  final HttpClient userClient = HttpClient();
 
   LightshotParser._();
 
   factory LightshotParser(
-      {required photosDirectory,
-      required databaseDirectory,
-      bool downloading = true}) {
+      {required Directory photosDirectory,
+      required Directory databaseDirectory}) {
     if (_instance.alreadyExists == true) {
-      _instance.downloading = downloading;
       log("The instance of LightshotParser already exists");
     } else {
       // It is necessary in order not to be banned immediately
-      HttpClient client = HttpClient();
-      client.userAgent =
+      _instance.userClient.userAgent =
           'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0';
+      _instance.userClient.idleTimeout = Duration(seconds: 5);
+      _instance.userClient.connectionTimeout = Duration(seconds: 5);
+      _instance.userClient.maxConnectionsPerHost = 1;
       if (!photosDirectory.existsSync()) {
         photosDirectory.createSync(recursive: true);
       }
@@ -42,95 +52,67 @@ class LightshotParser {
   }
 
   ///downloading image from {prnt.sc} url
-  getImage(Uri url) async {
+  Future<void> getImage(Uri url) async {
+    late final HttpClientResponse responseFromSite;
     try {
-      final responseFromSite = await http.get(url);
-      final RegExp imgPattern = RegExp(r'https.*((png)|(jpg)|(jpeg))');
-
-      switch (responseFromSite.statusCode) {
-        case 503:
-          await Future.delayed(const Duration(seconds: 10));
-          throw Exception('Server wants to ban this IP. Waiting');
-        case 403:
-          throw Exception('IP got banned');
-        case 200:
-          break;
-        default:
-          throw Exception(
-              'Can\'t reach server : ${responseFromSite.statusCode}');
-      }
-      final sourceCode = responseFromSite.body;
-
-      //looking for a direct address to the file
-      var imageStringUrl = imgPattern.stringMatch(sourceCode);
-
-      if (imageStringUrl == null) {
-        throw Exception("The photo is missing");
-      }
-
-      //Cutting the image url after .jpg or .png
-      imageStringUrl = imageStringUrl.substring(0, imageStringUrl.indexOf('"'));
-
-      if (!imageStringUrl.contains(imgPattern)) {
-        throw Exception("The photo is missing");
-      }
-
-      var imageUrl = Uri.parse(imageStringUrl);
-      final responseFromImg = await http.get(imageUrl);
-
-      if (responseFromImg.statusCode != 200) {
-        throw Exception(
-            "The photo is missing with ${responseFromImg.statusCode}");
-      }
-
-      //We filter out the imgur stubs that have 503 bites size
-      if (responseFromImg.bodyBytes.length == 503) {
-        throw Exception("The photo $imageStringUrl is imgur stub");
-      }
-
-      //Download the photo
-      await File("${photosDirectory.path}/"
-              '${url.pathSegments[url.pathSegments.length - 1]}'
-              '${imageStringUrl.substring(imageStringUrl.lastIndexOf('.'))}')
-          .writeAsBytes(responseFromImg.bodyBytes);
-
-      log('file $imageStringUrl from $url downloaded successful');
-      return 1;
+      responseFromSite = await (await userClient.getUrl(url)).close();
     } catch (e) {
-      log("There is an exception. $e with $url");
-      return 0;
+      throw CouldntConnectException('No vpn');
     }
+    final RegExp imgPattern = RegExp(r'https.*((png)|(jpg)|(jpeg))');
+
+    switch (responseFromSite.statusCode) {
+      case 503:
+        throw CouldntConnectException('Server wants to ban this IP');
+      case 403:
+        throw CouldntConnectException('IP got banned');
+      case 200:
+        break;
+    }
+    final sourceCode = await responseFromSite.transform(utf8.decoder).join();
+
+    //looking for a direct address to the file
+    var imageStringUrl = imgPattern.stringMatch(sourceCode);
+
+    if (imageStringUrl == null) {
+      throw NoPhotoException("The photo is missing");
+    }
+
+    //Cutting the image url after .jpg or .png
+    imageStringUrl = imageStringUrl.substring(0, imageStringUrl.indexOf('"'));
+
+    if (!imageStringUrl.contains(imgPattern)) {
+      throw NoPhotoException("The photo is missing");
+    }
+
+    var imageUrl = Uri.parse(imageStringUrl);
+    final responseFromImg = await http.get(imageUrl);
+
+    if (responseFromImg.statusCode != 200) {
+      throw NoPhotoException(
+          "The photo is missing with ${responseFromImg.statusCode}");
+    }
+
+    //We filter out the imgur stubs that have 503 bites size
+    if (responseFromImg.bodyBytes.length == 503) {
+      throw NoPhotoException("The photo $imageStringUrl is imgur stub");
+    }
+
+    //Download the photo
+    await File("${photosDirectory.path}/"
+            '${url.pathSegments[url.pathSegments.length - 1]}'
+            '${imageStringUrl.substring(imageStringUrl.lastIndexOf('.'))}')
+        .writeAsBytes(responseFromImg.bodyBytes);
+
+    log('file $imageStringUrl from $url downloaded successful');
   }
 
-  /// Start parsing
-  ///
-  /// Downloads numOfImages in new if newAddresses urls
-  /// starting from startingUrl. If startingUrl == '' uses random Url
-  /// iterator, otherwise - contractor Url iterator
-  Future<bool> parse(
-      int numOfImages, bool newAddresses, String startingUrl) async {
-    var getUrl = startingUrl == ''
-        ? gen.GetRandomUrl(newAddresses)
-        : gen.GetNextUrl(newAddresses, startingUrl);
-
-    // delay is necessary in order not to be banned
-    for (num i = 0; i < numOfImages;) {
-      if (database.urlInDb(getUrl.current)) {
-        log('The page ${getUrl.current} is already checked');
-        getUrl.moveNext();
-        continue;
-      }
-      i += await getImage(getUrl.current);
-      database.addUrlRecord(getUrl.current);
-      getUrl.moveNext();
-      if (!downloading) {
-        log('Parsing is canceled');
-        numOfImages = i.toInt();
-        break;
-      }
+  Future<bool> downloadOneImage(Uri imageUrl) async {
+    if (database.urlInDb(imageUrl)) {
+      log('The page $imageUrl is already checked');
+      return false;
     }
-    log("Successfully downloaded $numOfImages photos");
-    downloading = false;
+    await getImage(imageUrl);
     return true;
   }
 }
