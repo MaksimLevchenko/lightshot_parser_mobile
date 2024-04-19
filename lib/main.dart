50026bfbdeae9eb8e7ce605e6629120542eeb4bf
EliteNoobest <Valvekat@mail.ru>
2024-04-19 15:18:45 +0300

    Initialize intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:lightshot_parser_mobile/pages/main_page.dart';
+
+import 'generated/l10n.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
+    localizationsDelegates: const [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+      S.delegate,
+    ],
+    supportedLocales: S.delegate.supportedLocales,
     home: MainPage(),
     theme: ThemeData.from(
         useMaterial3: false,
