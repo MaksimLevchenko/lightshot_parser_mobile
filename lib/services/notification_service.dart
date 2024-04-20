62966a8aa02058766d7aee9e0dfcd4cfeff12e8e
EliteNoobest <Valvekat@mail.ru>
2024-04-20 04:32:26 +0300

    Fix import in notification_service.dart

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:lightshot_parser_mobile/generated/l10n.dart';
 
 class NotificationService {
   final FlutterLocalNotificationsPlugin notificationsPlugin =
